85aa50f3e52ee2ca15c6c825c5bf8e2acccdaf92
Ahmos-theking <you@example.com>
2024-02-08 01:26:00 +0200

    second commit

--- src/Componants/Item/Item.jsx
+++ src/Componants/Item/Item.jsx
@@ -3,26 +3,50 @@ import Layer from "../Layer/Layer";
 import { useState } from "react";
 
 export default function Item() {
-  const [display, setDisplay] = useState(true);
-
   return (
     <>
-      <Layer status={display} />
-      <div
-        className="col-md-4 position-relative overflow-hidden"
-        onClick={(e) => setDisplay(false)}
-      >
-        <div className="overly rounded-3 d-flex justify-content-center align-items-center ">
+      <div className="col-md-4 position-relative overflow-hidden">
+        <div
+          className="overly rounded-3 d-flex justify-content-center align-items-center "
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
           <i class="fa-solid fa-plus text-white "></i>
         </div>
         <img
           src="https://routeegy.github.io/startFramework/assets/images/poert1.png"
           alt=""
-          className="w-100 rounded-3 img-item "
+          className="w-100 rounded-3 img-item"
         />
+
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body">
+                <img
+                  src="https://routeegy.github.io/startFramework/assets/images/poert1.png"
+                  alt
+                  className="w-100"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
       <div className="col-md-4 position-relative overflow-hidden">
-        <div className="overly rounded-3 d-flex justify-content-center align-items-center ">
+        <div
+          className="overly rounded-3 d-flex justify-content-center align-items-center "
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal1"
+        >
           <i class="fa-solid fa-plus text-white "></i>
         </div>
         <img
@@ -30,9 +54,37 @@ export default function Item() {
           alt=""
           className="w-100 rounded-3 img-item"
         />
+
+
+<div
+          className="modal fade"
+          id="exampleModal1"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel1"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body">
+                <img
+                  src="https://routeegy.github.io/startFramework/assets/images/port2.png"
+                  alt
+                  className="w-100"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
+
       <div className="col-md-4 position-relative overflow-hidden">
-        <div className="overly rounded-3 d-flex justify-content-center align-items-center ">
+        <div
+          className="overly rounded-3 d-flex justify-content-center align-items-center "
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal2"
+        >
           <i class="fa-solid fa-plus text-white "></i>
         </div>
         <img
@@ -40,6 +92,26 @@ export default function Item() {
           alt=""
           className="w-100 rounded-3 img-item"
         />
+
+<div
+          className="modal fade"
+          id="exampleModal2"
+          tabIndex={-1}
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-body">
+                <img
+                  src="https://routeegy.github.io/startFramework/assets/images/port3.png"
+                  alt
+                  className="w-100"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
